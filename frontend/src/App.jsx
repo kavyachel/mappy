466359d6fad9d800aa3ecb06391f29f6ff63b257
Kavya Subramanian <kavyasub08@gmail.com>
2026-02-06 11:09:43 -0500

    oops

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,20 @@
-import { useRef, useEffect, useState } from 'react'
+import { useRef, useEffect } from 'react'
 import mapboxgl from 'mapbox-gl'
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './App.css'
+import { fetchPins, addPin } from '../api/pins.js';
 
-const MAPBOX_TOKEN = 'dont_commit_a_secret_challenge'
 function App() {
   const mapRef = useRef()
   const mapContainerRef = useRef()
 
+  const handleMapClick = ({e, map}) => {
+    const { lng, lat } = e.lngLat;
+    addPin({ map, lng, lat });
+  }
+
   useEffect(() => {
-    mapboxgl.accessToken = MAPBOX_TOKEN
+    mapboxgl.accessToken = import.meta.env.VITE_PUBLIC_MAPBOX_TOKEN;
 
     // Grab the user's location
     const geolocate = new mapboxgl.GeolocateControl({
@@ -27,14 +32,26 @@ function App() {
       container: mapContainerRef.current,
       interactive: true,
       dragPan: true,
-      zoom: 16,
+      zoom: 12,
     });
 
     mapRef.current.addControl(geolocate);
 
     mapRef.current.on('load', () => {
       geolocate.trigger();
+      
+      // Wait for geolocate to finish before we fetch pins
+      setTimeout(() => {
+        fetchPins(mapRef.current);
+      }, 1000);
     });
+
+    // Fetch pins when map moves/zooms
+    mapRef.current.on('moveend', () => {
+      fetchPins(mapRef.current);
+    });
+
+    mapRef.current.on('click', (e) => handleMapClick({e, map: mapRef.current}));
 
     return () => {
       mapRef.current.remove()
